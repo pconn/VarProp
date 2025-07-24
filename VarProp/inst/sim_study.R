747b51f7fe998893e9bd82b42438e54309fab595
David L Miller <dave.miller@bioss.ac.uk>
2025-07-24 08:27:48 +0100

    changes from review:
    - make prediction faster by using fit=FALSE and "G" objects
    - make plots have confidence bands
    - do plot smoothing within ggplot2

--- VarProp/inst/sim_study.R
+++ VarProp/inst/sim_study.R
@@ -13,7 +13,7 @@ gen_2_covs <- function(n_x,n_y,B1=0.5) {
   n=n_x*n_y
   Mu1 = t(L) %*% rnorm(n)
   Mu2 = t(L) %*% rnorm(n)
-  
+
   Covs= matrix(Mu1,n,2)
   Covs[,2]=B1*Mu1 + (1-abs(B1))*Mu2
   Covs 
@@ -56,20 +56,6 @@ preddata = data.frame(XY)
 preddata$area = 4
 pred_df=preddata
 pred_df$off.set=log(preddata$area)
-
-# show bug in prediction grid
-#  plotseg <- segdata
-#  plotseg$xmin <- plotseg$x - plotseg$Effort/2
-#  plotseg$xmax <- plotseg$x + plotseg$Effort/2
-#  plotseg$ymin <- plotseg$y - plotseg$Effort/2
-#  plotseg$ymax <- plotseg$y + plotseg$Effort/2
-#
-#  ggplot(plotseg) +
-#    geom_point(aes(x=x, y=y)) +
-#    geom_segment(aes(x=xmin, xend=xmax, y=y)) +
-#    geom_segment(aes(y=ymin, yend=ymax, x=x)) +
-#    geom_point(aes(x=x, y=y), data=preddata, colour="red") +
-#    theme_minimal()
 
 
 Sampled_xy=expand.grid(y=c(1,4,7,10,13,16,19,22,25),x=c(1,4,7,10,13,16,19,22,25))
@@ -118,7 +104,7 @@ for(isim in 1:n_sim){
                        data=dist_data)
   Beta_hat = sim_ddf$par
   VC_hat = solve(sim_ddf$hessian)
-  
+
   #fit base DSM
   obsdata = dist_data
   SegID_all_animals = rep(c(1:n_sample),N_covered)
@@ -132,6 +118,21 @@ for(isim in 1:n_sim){
   pred_det <- predict(sim_ddf,newdata=segdata)$fitted
   wts_pinv <- 1/(pred_det)
   wts_pinv = wts_pinv/mean(wts_pinv)  #make weights sum to n
+
+## show bug in prediction grid
+#plotseg <- segdata
+#plotseg$xmin <- plotseg$x - plotseg$Effort/2
+#plotseg$xmax <- plotseg$x + plotseg$Effort/2
+#plotseg$ymin <- plotseg$y - plotseg$Effort/2
+#plotseg$ymax <- plotseg$y + plotseg$Effort/2
+#
+#ggplot(plotseg) +
+#  geom_point(aes(x=x, y=y)) +
+#  geom_segment(aes(x=xmin, xend=xmax, y=y)) +
+#  geom_segment(aes(y=ymin, yend=ymax, x=x)) +
+#  geom_point(aes(x=x, y=y), data=preddata, colour="red") +
+#  theme_minimal()
+
 
   # dsm_flat <- dsm(count~1, ddf.obj=sim_ddf, 
   #                 segment.data=segdata, observation.data=obsdata, 
@@ -151,7 +152,7 @@ for(isim in 1:n_sim){
   dsm_k8_ht <- dsm(abundance.est~te(x,y,k=8), ddf.obj=sim_ddf, 
                    segment.data=segdata, observation.data=obsdata, 
                    method="REML")
-  
+
   #predicted abundance / bias
   dsm_k5_pred <- predict(dsm_k5, preddata, preddata$area)
   Ests[1,isim] = sum(dsm_k5_pred[Which_no_sample])
@@ -180,20 +181,20 @@ for(isim in 1:n_sim){
   CV[2,isim]=summary(var1_k8)$cv
   cur_CI = lognormal_CI(Ests[2,isim],CV=CV[2,isim])
   Coverage[2,isim]=(N_true_uncovered>cur_CI[1] & N_true_uncovered<cur_CI[2])
-  
+
   var1_k8_wt <- dsm_var_gam(dsm_k8_wt, pred.data=preddata.varprop,
                          off.set=preddata$area[Which_no_sample])
   CV[3,isim]=summary(var1_k8_wt)$cv
   cur_CI = lognormal_CI(Ests[3,isim],CV=CV[3,isim])
   Coverage[3,isim]=(N_true_uncovered>cur_CI[1] & N_true_uncovered<cur_CI[2])
-  
+
   var1_k8_ht <- dsm_var_gam(dsm_k8_ht, pred.data=preddata.varprop,
                             off.set=preddata$area[Which_no_sample])
   CV[4,isim]=summary(var1_k8_ht)$cv
   cur_CI = lognormal_CI(Ests[4,isim],CV=CV[4,isim])
   Coverage[4,isim]=(N_true_uncovered>cur_CI[1] & N_true_uncovered<cur_CI[2])
-  
-  
+
+
   # varprop method
   preddata$off.set=preddata$area
   var2_k8 <- dsm_varprop(dsm_k8,newdata=preddata[Which_no_sample,])
@@ -205,16 +206,17 @@ for(isim in 1:n_sim){
   Est_array[isim,,5]=dsm_varprop(dsm_k8,newdata=preddata)$pred
   Varprop_diagnostics[,isim]=(summary(var2_k8)$varprop_diagnostic)[[1]][,4]-
     (summary(var2_k8)$varprop_diagnostic)[[1]][,2]
-  
+
   # #varprop using original fit for point estimate
   # CV[3,isim]=sqrt(var2_k8$var)/Ests[1,isim]
   # cur_CI = lognormal_CI(Ests[1,isim],CV=CV[3,isim])
   # Coverage[3,isim]=(N_true_uncovered>cur_CI[1] & N_true_uncovered<cur_CI[2])
-  
+
   #constructive bootstrap
   gam_cv = summary(var1_k8)$gam.cv
-  Count_df = data.frame("count"=tabulate(obsdata$Sample.Label,nbins=n_sample),
-            "x"=Sampled_xy$x,"y"=Sampled_xy$y)
+  Count_df = data.frame(count=tabulate(obsdata$Sample.Label,nbins=n_sample),
+                        x=Sampled_xy$x,
+                        y=Sampled_xy$y)
   # Sigma_hat = exp(Beta_hat[1]+Beta_hat[2]*Cov_array[isim,Sampled,2])
   # P_hat = (pnorm(Ones_boot,Zeros_boot,Sigma_hat)-0.5)/
   #   dnorm(Zeros_boot,Zeros_boot,Sigma_hat)
@@ -230,15 +232,18 @@ for(isim in 1:n_sim){
 
 
   # only need to setup basis/penalties once!
-    # setup count model
-    Count_df$off.set = 1#log(P_boot)+log(4) # p * area on real scale
-    gam_boot_G = gam(count~offset(off.set)+te(x,y,k=8),family="quasipoisson",
-                   data=Count_df,method="REML", fit=FALSE)
-    # HT model
-    Count_df$ht = Count_df$count#/P_boot
-    Count_df$off.set = log(4)
-    gam_boot_ht_G = gam(ht~offset(off.set)+te(x,y,k=8),family="quasipoisson",
-                   data=Count_df,method="REML", fit=FALSE)
+  # can use the fit=FALSE to build necessary objects, then correct the
+  # offsets or responses inside the loop
+
+  # setup count model
+  Count_df$off.set <- 1#log(P_boot)+log(4) # p * area on real scale
+  gam_boot_G <- gam(count~offset(off.set)+te(x,y,k=8),family="quasipoisson",
+                 data=Count_df,method="REML", fit=FALSE)
+  # setup HT model
+  Count_df$ht <- Count_df$count#/P_boot
+  Count_df$off.set <- log(4)
+  gam_boot_ht_G <- gam(ht~offset(off.set)+te(x,y,k=8),family="quasipoisson",
+                 data=Count_df,method="REML", fit=FALSE)
 
   for(iboot in 1:n_boot){
     Beta_boot = rmvnorm(1,Beta_hat,VC_hat)
@@ -250,22 +255,22 @@ for(isim in 1:n_sim){
     # count model
     gam_boot_G$offset = log(P_boot)+log(4) # p * area on real scale
     gam_boot = gam(method="REML", G=gam_boot_G)
-
-    gam_pred = predict(gam_boot,newdata=pred_df[Which_no_sample,],type="response")
+    gam_pred = predict(gam_boot, newdata=pred_df[Which_no_sample,],
+                       type="response")
     N_boot[iboot]=sum(gam_pred)
 
     #H-T model
     gam_boot_ht_G$y = Count_df$count/P_boot
     gam_boot = gam(method="REML", G=gam_boot_ht_G)
-
-    gam_pred = predict(gam_boot,newdata=pred_df[Which_no_sample,],type="response")
+    gam_pred = predict(gam_boot, newdata=pred_df[Which_no_sample,],
+                       type="response")
     N_boot_ht[iboot]=sum(gam_pred)
   }
   var_Boot = var(N_boot)+(summary(var1_k8)$gam.cv*Ests[2,isim])^2
   CV[6,isim] = sqrt(var_Boot)/Ests[2,isim]
   cur_CI = lognormal_CI(Ests[2,isim],CV=CV[6,isim])
   Coverage[6,isim]=(N_true_uncovered>cur_CI[1] & N_true_uncovered<cur_CI[2])
-  
+
   var_Boot = var(N_boot_ht)+(summary(var1_k8_ht)$gam.cv*Ests[4,isim])^2
   CV[7,isim] = sqrt(var_Boot)/Ests[4,isim]
   cur_CI = lognormal_CI(Ests[4,isim],CV=CV[7,isim])
@@ -293,77 +298,82 @@ save.image('sim_results_psp.RData')
 # ggplot(Plot_df)+geom_tile(aes(x=x,y=y,fill=RelBias_sp))
 # 
 
-Emp_cor <- rep(NA,1000)
-for(isim in 1:1000)Emp_cor[isim]=cor(Cov_array[isim,,2],Est_array[isim,,2])
+Emp_cor <- rep(NA,n_sim)
+for(isim in 1:n_sim)Emp_cor[isim]=cor(Cov_array[isim,,2],Est_array[isim,,2])
 
 Plot_df = data.frame("Emp_cor"=Emp_cor,"True_cor"=Cor_vec)
 ggplot(Plot_df)+geom_point(aes(x=True_cor,y=Emp_cor))+xlab(expression(rho(bold(x)[hab],bold(x)[p])))+
   ylab(expression(hat(rho)(hat(bold(N))[s],bold(x)[p])))
 png(filename="Cor_scatter.png",width=5,height=5,units='in',res=600)
-ggplot(Plot_df)+geom_point(aes(x=True_cor,y=Emp_cor))+xlab(expression(rho(bold(x)[hab],bold(x)[p])))+
+ggplot(Plot_df)+
+  geom_point(aes(x=True_cor,y=Emp_cor))+
+  xlab(expression(rho(bold(x)[hab],bold(x)[p])))+
   ylab(expression(hat(rho)(hat(bold(N))[s],bold(x)[p])))
 dev.off()
 
 
-
-Cor_pred = c(-100:100)/100
-n_pred = length(Cor_pred)
-Cov_mod_list = CV_mod_list= vector("list",4)
-Bias_mod_list =  vector("list",5)
-new_data = data.frame(Cor=Cor_pred)
+# create one data.frame for all the models and then
+# smooth using ggplot2
+gam_df <- c()
+proc <- c("k=5,delta", "delta", "wtd,delta", "HT,delta", "varprop")
 for(i in 1:5){
-  gam_df = data.frame("Cor"=Cor_vec) #,"Coverage"=Coverage[i,],"CV"=CV[i,])
-  gam_df$Bias = Bias[i,]
-  gam_bias = gam(Bias~s(Cor,k=4),data=gam_df)
-  Bias_mod_list[[i]]=predict(gam_bias,newdata=new_data,type="response")
-}
-for(i in 1:7){
-  gam_df = data.frame("Cor"=Cor_vec,"Coverage"=Coverage[i,],"CV"=CV[i,])
-  gam_cov = gam(Coverage~s(Cor,k=4),data=gam_df,family="binomial")
-  gam_cv = gam(CV~s(Cor,k=4),data=gam_df)
-  Cov_mod_list[[i]]=predict(gam_cov,newdata=new_data,type="response")
-  CV_mod_list[[i]]=predict(gam_cv,newdata=new_data,type="response")
+  gam_df <- rbind(gam_df,
+                  data.frame(Cor=Cor_vec,
+                             Coverage=as.numeric(Coverage[i,]),
+                             CV=CV[i,],
+                             Bias = Bias[i,],
+                             Procedure=proc[i]))
 }
 
-Plot_df = data.frame("Correlation"=rep(Cor_pred,7),
-                     "Coverage"=unlist(Cov_mod_list),
-                     "Procedure"=c(rep("k=5,delta",n_pred),rep("delta",n_pred),
-                                   rep("wtd,delta",n_pred),rep("HT,delta",n_pred),
-                                   rep("varprop",n_pred),rep("boot",n_pred),
-                                   rep("HT,boot",n_pred)))
-ggplot(Plot_df)+geom_line(aes(x=Correlation,y=Coverage,color=Procedure),size=1.5)+
-  scale_color_viridis(discrete=TRUE)
-png(filename="Sim_coverage.png",width=5,height=5,units='in',res=600)
-ggplot(Plot_df)+geom_line(aes(x=Correlation,y=Coverage,color=Procedure),size=1.5)+
-  scale_color_viridis(discrete=TRUE)
-dev.off()
+# bias plot
+ggplot(gam_df)+
+  geom_smooth(aes(x=Cor, y=Bias, group=Procedure,
+                  colour=Procedure, fill=Procedure),
+              method="gam", formula=y~s(x, k=4), se=TRUE,
+              size=1.5, alpha=0.25)+
+  theme_minimal() +
+  scale_fill_viridis_d() +
+  scale_color_viridis_d()
 
-Plot_df = data.frame("Correlation"=rep(Cor_pred,7),
-                     "CV"=unlist(CV_mod_list),
-                     "Procedure"=c(rep("k=5,delta",n_pred),rep("delta",n_pred),
-                                   rep("wtd,delta",n_pred),rep("HT,delta",n_pred),
-                                   rep("varprop",n_pred),rep("boot",n_pred),
-                                   rep("HT,boot",n_pred)))
-ggplot(Plot_df)+geom_line(aes(x=Correlation,y=CV,color=Procedure),size=1.5)+
-  scale_color_viridis(discrete=TRUE)
+# coverage
+ggplot(gam_df)+
+  geom_smooth(aes(x=Cor, y=Coverage, group=Procedure,
+                  colour=Procedure, fill=Procedure),
+              method="gam", formula=y~s(x, k=4), se=TRUE,
+              method.args=list(family="binomial"),
+              size=1.5, alpha=0.25)+
+  theme_minimal() +
+  scale_fill_viridis_d() +
+  scale_color_viridis_d()
 
-png(filename="Sim_CV.png",width=5,height=5,units='in',res=600)
-ggplot(Plot_df)+geom_line(aes(x=Correlation,y=CV,color=Procedure),size=1.5)+
-  scale_color_viridis(discrete=TRUE)
-dev.off()
-
-Plot_df = data.frame("Correlation"=rep(Cor_pred,5),
-                     "Bias"=unlist(Bias_mod_list),
-                     "Procedure"=c(rep("k=5",n_pred),rep("base",n_pred),
-                                                rep("wtd",n_pred),rep("HT",n_pred),
-                                                rep("varprop",n_pred)))
-ggplot(Plot_df)+geom_line(aes(x=Correlation,y=Bias,color=Procedure),linewidth=1.5)+
-  scale_color_viridis(discrete=TRUE)
-png(filename="Sim_bias.png",width=5,height=5,units='in',res=600)
-ggplot(Plot_df)+geom_line(aes(x=Correlation,y=Bias,color=Procedure),linewidth=1.5)+
-  scale_color_viridis(discrete=TRUE)
-dev.off()
+# CV
+ggplot(gam_df)+
+  geom_smooth(aes(x=Cor, y=CV, group=Procedure,
+                  colour=Procedure, fill=Procedure),
+              method="gam", formula=y~s(x, k=4), se=TRUE,
+              size=1.5, alpha=0.5)+
+  theme_minimal() +
+  scale_fill_viridis_d() +
+  scale_color_viridis_d()
 
 
+# looking at those plots with the data too
+ggplot(data=gam_df, aes(x=Bias, y=CV))+
+  geom_smooth(aes(colour=Procedure, fill=Procedure),
+              method="gam", formula=y~s(x, k=4), se=TRUE,
+              size=1.5, alpha=0.25)+
+  geom_point() +
+  facet_wrap(~Procedure) +
+  theme_minimal() +
+  scale_fill_viridis_d() +
+  scale_color_viridis_d()
 
-#check out correlation between estimated surface and estimated detection
+ggplot(data=gam_df, aes(x=Cor, y=CV))+
+  geom_smooth(aes(colour=Procedure, fill=Procedure),
+              method="gam", formula=y~s(x, k=4), se=TRUE,
+              size=1.5, alpha=0.25)+
+  geom_point() +
+  facet_wrap(~Procedure) +
+  theme_minimal() +
+  scale_fill_viridis_d() +
+  scale_color_viridis_d()
